caa110d18e52706f5b0ea7adab8cd93c94582b93
Rahul Dixit <dixit.rahul94@gmail.com>
2021-06-18 14:03:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Demo App confguration to netlify is completed.
         </p>
         <a
           className="App-link"
